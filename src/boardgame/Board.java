93245dde5a116054cf099a2a319d70f076c83a5e
Leandro Torres <leandroftm@gmail.com>
2020-08-13 15:32:07 -0300

    Little improvement in board printing

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -48,7 +48,7 @@ public class Board {
 	public void placePiece(Piece piece, Position position) {
 		if (thereIsAPiece(position)) {
 
-			throw new BoardException("There is alread a piece on position " + position);
+			throw new BoardException("There is already a piece on position " + position);
 
 		}
 		pieces[position.getRow()][position.getColumn()] = piece;
