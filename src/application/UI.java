d055de68777ac8aea6cee733e82e074db6a62b33
leandroftm <leandroftm@gmail.com>
2020-11-16 16:52:18 -0300

    Checkmate logic

--- src/application/UI.java
+++ src/application/UI.java
@@ -35,7 +35,7 @@ public class UI {
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
 	public static void clearScreen() {
-		//https://stackoverflow.com/questions/2979383/java-clear-the-console
+		// https://stackoverflow.com/questions/2979383/java-clear-the-console
 		System.out.print("\033[H\033[2J");
 		System.out.flush();
 	}
@@ -57,18 +57,25 @@ public class UI {
 	}
 
 	public static void printMatch(ChessMatch chessMatch, List<ChessPiece> captured) {
-		
+
 		printBoard(chessMatch.getPieces());
 		System.out.println();
 		printCapturedPieces(captured);
 		System.out.println();
 		System.out.println("Turn : " + chessMatch.getTurn());
-		System.out.println("Waiting player " + chessMatch.getCurrentPlayer());
-		if(chessMatch.getCheck()) {
-			System.out.println("CHECK!");
+		if (!chessMatch.getCheckMate()) {
+
+			System.out.println("Waiting player " + chessMatch.getCurrentPlayer());
+			if (chessMatch.getCheck()) {
+				System.out.println("CHECK!");
+			}
+		}
+		else {
+			System.out.println("CHECKMATE!");
+			System.out.println("Winner: " + chessMatch.getCurrentPlayer());
 		}
 	}
-	
+
 	public static void printBoard(ChessPiece[][] pieces) {
 
 		for (int i = 0; i < pieces.length; i++) {
@@ -86,7 +93,7 @@ public class UI {
 		System.out.println("  a b c d e f g h ");
 	}
 
-	public static void printBoard(ChessPiece[][] pieces, boolean [][] possibleMoves) {
+	public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
 
 		for (int i = 0; i < pieces.length; i++) {
 
@@ -102,11 +109,11 @@ public class UI {
 
 		System.out.println("  a b c d e f g h ");
 	}
-	
+
 	private static void printPiece(ChessPiece piece, boolean background) {
-		
-		if(background) {
-			
+
+		if (background) {
+
 			System.out.print(ANSI_BLUE_BACKGROUND);
 		}
 
@@ -123,9 +130,11 @@ public class UI {
 	}
 
 	private static void printCapturedPieces(List<ChessPiece> captured) {
-		
-		List<ChessPiece> white = captured.stream().filter(x -> x.getColor() == Color.WHITE).collect(Collectors.toList());
-		List<ChessPiece> black = captured.stream().filter(x -> x.getColor() == Color.BLACK).collect(Collectors.toList());
+
+		List<ChessPiece> white = captured.stream().filter(x -> x.getColor() == Color.WHITE)
+				.collect(Collectors.toList());
+		List<ChessPiece> black = captured.stream().filter(x -> x.getColor() == Color.BLACK)
+				.collect(Collectors.toList());
 		System.out.println("Captured pieces: ");
 		System.out.println("White: ");
 		System.out.print(ANSI_WHITE);
